c6175b70a5cc035363dc3cb70a10479986d80b9f
hpuser <hpuser@LAPTOP-N261TKEL>
2017-08-11 21:27:25 -0400

    Added all of the remaining functions into the calculator.

---
+++ CalcGUI/CalculatorForm.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcGUI
+{
+    public partial class CalculatorForm : Form
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private bool _isDecimalClicked;
+
+        private string _currentOperator;
+
+        private List<double> _operandList;
+
+        private double _result;
+
+        private bool _isOperandTwo;
+
+        // PUBLIC PROPERTIES
+        public bool IsDecimalClicked
+        {
+
+            get
+            {
+                return this._isDecimalClicked;
+            }
+
+            set
+            {
+                this._isDecimalClicked = value;
+            }
+
+        }
+
+        public string CurrentOperator
+        {
+
+            get
+            {
+                return this._currentOperator;
+            }
+
+            set
+            {
+                this._currentOperator = value;
+            }
+
+        }
+
+        public List<double> OperandList
+        {
+
+            get
+            {
+                return this._operandList;
+            }
+
+            set
+            {
+                this._operandList = value;
+            }
+
+        }
+
+        public double Result
+        {
+
+            get
+            {
+                return this._result;
+            }
+
+            set
+            {
+                this._result = value;
+            }
+
+        }
+
+        public bool IsOperandTwo
+        {
+
+            get
+            {
+                return this._isOperandTwo;
+            }
+
+            set
+            {
+                this._isOperandTwo = value;
+            }
+        }
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the CalculatorForm class
+        /// </summary>
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is an event handler for the "FormClosing" event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// This is the shared event handler for the Calculator Buttons
+        /// Not including the Opertor Buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            Button calculatorButton = sender as Button; // downcasting
+
+            if ((this.IsDecimalClicked) && (calculatorButton.Text == "."))
+            {
+                return;
+            }
+
+            if (calculatorButton.Text == ".")
+            {
+                this.IsDecimalClicked = true;
+            }
+
+
+            if (ResultTextBox.Text == "0")
+            {
+                if (calculatorButton.Text == ".")
+                {
+                    ResultTextBox.Text += calculatorButton.Text;
+                }
+                else
+                {
+                    ResultTextBox.Text = calculatorButton.Text;
+                }
+            }
+            else
+            {
+                if ((OperandList.Count > 0) && (this._isOperandTwo == false))
+                {
+
+                    ResultTextBox.Text = calculatorButton.Text;
+                    this._isOperandTwo = true;
+
+                }
+                else
+                {
+                    ResultTextBox.Text += calculatorButton.Text;
+                }
+
+            }
+
+
+
+            //Debug.WriteLine("A Calculator Button was clicked");
+        }
+
+        /// <summary>
+        /// This is a shared event handler for the Operator Buttons of the calculator
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OperatorButton_Click(object sender, EventArgs e)
+        {
+            Button operatorButton = sender as Button; // downcasting
+
+            double operand = this._convertOperand(ResultTextBox.Text); // convert to number
+
+
+            switch (operatorButton.Text)
+            {
+                case "C":
+                    this._clear();
+                    break;
+                case "=":
+                    this._showResult(operand);
+                    break;
+                case "⌫":
+                    this._deleteText(operand);
+                    break;
+                case "±":
+                    this._plusMinus(operand);
+                    break;
+                default:
+                    this._calculate(operand, operatorButton.Text);
+                    break;
+            }
+
+
+        }
+
+        /// <summary>
+        /// This method shows the Result of the last operation in the ResultTextBox
+        /// </summary>
+        /// <param name="text"></param>
+        private void _showResult(double operand)
+        {
+            this._calculate(operand, this.CurrentOperator);
+            ResultTextBox.Text = this.Result.ToString();
+
+        }
+
+        private void _deleteText(double operand)
+        {
+            ResultTextBox.Text = ResultTextBox.Text.Remove(ResultTextBox.Text.Length - 1, 1);        
+        }
+
+        private void _plusMinus(double operand)
+        {
+            ResultTextBox.Text = "-" + ResultTextBox.Text;
+        }
+
+        /// <summary>
+        /// This method calculates the result of all the operands in the OperandList
+        /// </summary>
+        /// <param name="text1"></param>
+        /// <param name="text2"></param>
+        private void _calculate(double operand, string operatorString)
+        {
+
+            OperandList.Add(operand);
+            if (OperandList.Count > 1)
+            {
+                switch (operatorString)
+                {
+                    case "+":
+                        this.Result = this.OperandList[0] + this.OperandList[1];
+                        break;
+                    case "-":
+                        this.Result = this.OperandList[0] - this.OperandList[1];
+                        break;
+                    case "x":
+                        this.Result = this.OperandList[0] * this.OperandList[1];
+                        break;
+                    case "÷":
+                        this.Result = this.OperandList[0] / this.OperandList[1];
+                        break;
+
+                }
+                this.OperandList.Clear();
+                this.OperandList.Add(this.Result);
+                this.IsOperandTwo = false;
+            }
+
+            this.CurrentOperator = operatorString;
+        }
+
+        /// <summary>
+        /// This method converts the string from the ResultTextBox to a number
+        /// </summary>
+        /// <param name="operandString"></param>
+        /// <returns></returns>
+        private double _convertOperand(string operandString)
+        {
+            try
+            {
+                return Convert.ToDouble(operandString);
+            }
+            catch (Exception exception)
+            {
+
+                Debug.WriteLine("An Error Occurred");
+                Debug.WriteLine(exception.Message);
+            }
+            return 0;
+
+        }
+
+        /// <summary>
+        /// This is the private _clear method. It resets / clears the calculator.
+        /// </summary>
+        private void _clear()
+        {
+            this.IsDecimalClicked = false;
+            ResultTextBox.Text = "0";
+            this.CurrentOperator = "C";
+            this.OperandList = new List<double>();
+            this.IsOperandTwo = false;
+            this.Result = 0;
+        }
+
+        /// <summary>
+        /// This is the event handler for the "Load" event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorForm_Load(object sender, EventArgs e)
+        {
+            this._clear();
+        }
+    }
+}
--- CalcGUI/Program.cs
+++ CalcGUI/Program.cs
@@ -4,19 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*Name: Sahal Mohamed
+ * Student Number: 300895007
+ * Lab 26
+ */
 namespace CalcGUI
 {
-    static class Program
+    public static class Program
     {
+        // FORM INSTANTIATION
+        public static CalculatorForm calculatorForm;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CalculatorForm());
+            calculatorForm = new CalculatorForm(); // instantiates object of type Calculator Form
+
+
         }
     }
 }
